dcc0f89c14d37b9918b53fdd6c191903810c8c1b
jnfDev <jairo.jnf.2010@gmail.com>
2020-06-28 21:05:02 -0400

    Register CTP jnf_works

---
+++ jnf_myworks.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Plugin Name: My Works
+ * Autor: jnfDev
+ * Version: 1.0.0
+ * Text Domain: jnf_myworks
+ */
+
+function jnf_register_cpt() {
+    register_post_type('jnf_works', array(
+        'labels' => array(
+            'name' => __('Works', 'jnf_myworks'),
+            'singular_name' => __('Work', 'jnf_myworks')
+        ),
+        'public' => true,
+        'has_archive' => true,
+        'rewrite' => array(
+            'slug' => 'works'
+        ),
+    ));
+}
+
+add_action('init', 'jnf_register_cpt');
